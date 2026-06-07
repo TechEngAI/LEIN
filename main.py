2fef97fdf88389ba64cb5b59422493dfd1190476
TechEngAI <abdulhammedmuhammedawwal@gmail.com>
2026-06-07 23:28:54 +0100

    deployment

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ app = FastAPI(title="LEIN API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://lein-beta.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
     expose_headers=["*"],
 )
 
